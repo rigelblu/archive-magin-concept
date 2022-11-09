d72b40e458345ed9cf1c5144bb191c0364d6681a
Tom Hosiawa <tom@rigelblu.com>
2022-11-09 13:54:37 -0500

    enhance: center and style hello magin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,9 @@ import HTMLBody from '@/components/htmlElements/HTMLBody';
 export default function PageRoot() {
   return (
     <HTMLBody showHeader={false} showFooter={false}>
-      <div>hello magin</div>
+      <h1 className='flex h-screen justify-content-center align-items-center mgn-text-blue-rb'>
+        hello magin
+      </h1>
     </HTMLBody>
   );
 }
