0c2b8c8ddf6dbf563846e8f2c510c6c319c3c809
Tom Hosiawa <tom@rigelblu.com>
2022-12-24 22:43:50 -0500

    feat | home: create home site with copy and cta button

---
+++ src/pages/home.tsx
@@ -0,0 +1,22 @@
+// Copyright rigélblu inc.
+// All rights reserved.
+import { Button } from 'primereact/button';
+
+export default function Home() {
+  return (
+    <div className='flex flex-column h-screen justify-content-center align-items-center '>
+      <h1 className='text-center mb-8'>
+        Have you <span className='mgn-text-blue-rb'>read a novel</span> <br /> and wondered,
+        <br /> why can&apos;t I <span className='mgn-text-blue-rb'>remember</span>{' '}
+        <br className='inline sm:hidden' />
+        anything?
+      </h1>
+
+      <h2 className='text-center'>
+        Learn to watch a novel <br />
+        like a <span className='mgn-text-blue-rb'>Pixar</span> director
+      </h2>
+      <Button className='mgn-primary-cta'>Try magin</Button>
+    </div>
+  );
+}
