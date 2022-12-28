d263abab26a8778f5678586e3943211e9bed2ba8
Tom Hosiawa <tom@rigelblu.com>
2022-12-27 20:31:02 -0500

    enhance | MaginPreview: show Guide component between the Book and Film components

--- src/components/MaginPreview/MaginPreview.tsx
+++ src/components/MaginPreview/MaginPreview.tsx
@@ -2,11 +2,13 @@
 // All rights reserved.
 import Book from '@/components/Book/Book';
 import Film from '@/components/Film/Film';
+import Guide from '@/components/Guide/Guide';
 
 export default function MaginPreview() {
   return (
     <>
       <Book />
+          <Guide />
       <Film />
     </>
   );
