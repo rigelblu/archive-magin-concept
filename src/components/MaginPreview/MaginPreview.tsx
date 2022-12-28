5b6c6fb65c39ff409b75ba89ba71bbe272f5744d
Tom Hosiawa <tom@rigelblu.com>
2022-12-27 21:05:54 -0500

    feat | MaginPreview: show Navigation at bottom of screen

--- src/components/MaginPreview/MaginPreview.tsx
+++ src/components/MaginPreview/MaginPreview.tsx
@@ -3,16 +3,20 @@
 import Book from '@/components/Book/Book';
 import Film from '@/components/Film/Film';
 import Guide from '@/components/Guide/Guide';
+import Navigation from '../Navigation/Navigation';
 
 export default function MaginPreview() {
   return (
     // REFACTOR: convert into component css class
     <div className='flex w-screen h-screen p-3 justify-content-center align-items-center'>
       <div className='w-full h-full sm:max-w-24rem sm:max-h-40rem mgn-debug-outline-red'>
-        <div>
-          <Book />
-          <Guide />
-          <Film />
+        <div className='h-full flex flex-column justify-content-between'>
+          <div>
+            <Book />
+            <Guide />
+            <Film />
+          </div>
+          <Navigation />
         </div>
       </div>
     </div>
