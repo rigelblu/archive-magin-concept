6aa0aceba72a22fdbbe2edfefa85e1ce2daf3eb6
Tom Hosiawa <tom@rigelblu.com>
2023-01-09 00:39:51 -0500

    refactor | Navigation: accept button labels as props

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,17 +1,19 @@
 // Copyright rigélblu inc.
 // All rights reserved.
+
 interface Props {
+  left: string;
+  right: string;
   className?: string;
 }
 
 export default function Navigation(props: Props) {
-  const { className = '' } = props;
+  const { left, right, className = '' } = props;
 
   return (
     <div className={`mgn-navigation ${className} flex justify-content-between`}>
-      {/* REFACTOR: accept divs as props */}
-      <div className='text-center mgn-cta-secondary'>Left</div>
-      <div className='text-center mgn-cta-primary'>Right</div>
+      <div className='text-center mgn-cta-secondary'>{left}</div>
+      <div className='text-center mgn-cta-primary'>{right}</div>
     </div>
   );
 }
