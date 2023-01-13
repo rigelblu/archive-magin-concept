ec9820af57c5997e57611745aea459c61f55a237
Tom Hosiawa <tom@rigelblu.com>
2023-01-12 23:31:29 -0500

    enhance | MarginPreview: change bg color to white

--- src/pages/try-magin/2/index.tsx
+++ src/pages/try-magin/2/index.tsx
@@ -13,7 +13,7 @@ export default function MarginPreview() {
   const router = useRouter();
 
   return (
-    <div className='mgn-try-magin'>
+    <div className='mgn-try-magin bg-white'>
       {/* REFACTOR: convert into component css class */}
       <div className='mgn-preview flex w-screen h-screen p-3 justify-content-center align-items-center'>
         <div className='w-full h-full sm:max-w-24rem sm:max-h-40rem'>
