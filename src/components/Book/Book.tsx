c1f3b3c6033c47a209de985b865f2bffa077821c
Tom Hosiawa <tom@rigelblu.com>
2023-01-12 23:48:20 -0500

    enhance | Book: show page control based on props

--- src/components/Book/Book.tsx
+++ src/components/Book/Book.tsx
@@ -5,17 +5,20 @@ import Page from './Page/Page';
 
 interface Props {
   className?: string;
+  showPageControls?: boolean;
 }
 
 export default function Book(props: Props) {
-  const { className = '' } = props;
+  const { className = '', showPageControls = true } = props;
   const onClick = () => console.log('prev/next click');
 
   return (
     <div className={`mgn-book ${className} flex align-items-center`}>
+      {showPageControls && (
       <PageControl action='prev' isShown={false} onClick={onClick} className='px-1' />
+      )}
       <Page />
-      <PageControl action='next' isShown onClick={onClick} className='px-1' />
+      {showPageControls && <PageControl action='next' isShown onClick={onClick} className='px-1' />}
     </div>
   );
 }
