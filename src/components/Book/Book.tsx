2d0a04b7410e26554994ce926200f7225aa37b6a
Tom Hosiawa <tom@rigelblu.com>
2023-01-12 23:49:58 -0500

    enhance | Book: show page content page on step prop

--- src/components/Book/Book.tsx
+++ src/components/Book/Book.tsx
@@ -5,19 +5,20 @@ import Page from './Page/Page';
 
 interface Props {
   className?: string;
+  maginPreviewStep?: number | null;
   showPageControls?: boolean;
 }
 
 export default function Book(props: Props) {
-  const { className = '', showPageControls = true } = props;
+  const { className = '', maginPreviewStep = null, showPageControls = true } = props;
   const onClick = () => console.log('prev/next click');
 
   return (
     <div className={`mgn-book ${className} flex align-items-center`}>
       {showPageControls && (
-      <PageControl action='prev' isShown={false} onClick={onClick} className='px-1' />
+        <PageControl action='prev' isShown={false} onClick={onClick} className='px-1' />
       )}
-      <Page />
+      <Page maginPreviewStep={maginPreviewStep} />
       {showPageControls && <PageControl action='next' isShown onClick={onClick} className='px-1' />}
     </div>
   );
