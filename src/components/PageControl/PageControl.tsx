a5cfc2183b3d542f02b7c515f0e131a5085f9c74
Tom Hosiawa <tom@rigelblu.com>
2023-01-17 00:11:50 -0500

    enhance | PageControl: fix width to 1rem

--- src/components/PageControl/PageControl.tsx
+++ src/components/PageControl/PageControl.tsx
@@ -30,7 +30,7 @@ export default function PageControl(props: Props) {
   // TODO: style chevron in blue-rb and background in blue-rb-lighter
   return (
     <Button
-      className={`mgn-page-control p-button-text p-0 ${className}`}
+      className={`mgn-page-control w-1rem p-button-text p-0 ${className}`}
       aria-label={action}
       icon={`pi ${icon}`}
       onClick={onClick}
