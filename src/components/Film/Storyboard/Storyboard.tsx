4f0ae83150f94cb37311653b11cd2b0acda9caf6
Tom Hosiawa <tom@rigelblu.com>
2023-01-17 00:19:03 -0500

    enhance | Storyboard: wrap inside a Scene

--- src/components/Film/Storyboard/Storyboard.tsx
+++ src/components/Film/Storyboard/Storyboard.tsx
@@ -1,6 +1,7 @@
 // Copyright rigélblu inc.
 // All rights reserved.
 import Image from 'next/image';
+import Scene from '@/components/Scene/Scene';
 
 interface Props {
   className?: string;
@@ -13,12 +14,14 @@ export default function Storyboard(props: Props) {
     <div className={`mgn-storyboard w-full ${className}`}>
       {/* OPTIMIZE: load image cdn */}
       {/* TODO: load image based on selected scene */}
+      <Scene sceneNum={1}>
       <Image
         src='/assets/common/images/storyboard-placeholder.png'
         alt='Placeholder'
         width={330}
         height={270}
       />
+      </Scene>
     </div>
   );
 }
