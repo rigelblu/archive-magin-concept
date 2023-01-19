05b247b47ea5883af343b2aac29e0f91e94ab9d0
Tom Hosiawa <tom@rigelblu.com>
2023-01-19 12:58:29 -0500

    refactor | Storyboard: move PageControl to Film

--- src/components/Film/Film.tsx
+++ src/components/Film/Film.tsx
@@ -1,6 +1,8 @@
 // Copyright rigélblu inc.
 // All rights reserved.
+import PageControl from '@/components/PageControl/PageControl';
 import Scene from '@/components/Scene/Scene';
+
 import Storyboard from './Storyboard/Storyboard';
 
 interface Props {
@@ -13,9 +15,11 @@ export default function Film(props: Props) {
   return (
     // REFACTOR: move px-2 to global, align with book
     <div className={`mgn-film flex px-3 ${className}`}>
+      <PageControl action='prev' onClick={() => {}} className='px-1' isShown={false} />
       <Scene sceneNum={1}>
         <Storyboard />
       </Scene>
+      <PageControl action='next' onClick={() => {}} className='px-1' />
     </div>
   );
 }
--- src/components/Film/Storyboard/Storyboard.tsx
+++ src/components/Film/Storyboard/Storyboard.tsx
@@ -2,7 +2,6 @@
 // All rights reserved.
 import Image from 'next/image';
 import Scene from '@/components/Scene/Scene';
-import PageControl from '@/components/PageControl/PageControl';
 
 interface Props {
   className?: string;
@@ -12,20 +11,17 @@ export default function Storyboard(props: Props) {
   const { className = '' } = props;
 
   return (
-    <div className={`mgn-storyboard flex align-items-center ${className}`}>
-      <PageControl action='prev' onClick={() => {}} isShown={false} className='px-1' />
+    <div className={`mgn-storyboard ${className}`}>
       {/* OPTIMIZE: load image cdn */}
       {/* TODO: load image based on selected scene */}
-      <Scene sceneNum={1}>
-        <Image
-          src='/assets/common/images/storyboard-placeholder.png'
-          alt='Placeholder'
-          className='!object-scale-down w-20rem h-auto'
-          width={330}
-          height={270}
-        />
-      </Scene>
-      <PageControl action='next' onClick={() => {}} className='px-1' />
+
+      <Image
+        src='/assets/common/images/storyboard-placeholder.png'
+        alt='Placeholder'
+        className='!object-scale-down w-20rem h-auto'
+        width={330}
+        height={270}
+      />
     </div>
   );
 }
