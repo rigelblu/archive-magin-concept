59d0b8d40f0e1de79365b2ca92a5caabed332278
Tom Hosiawa <tom@rigelblu.com>
2023-01-19 17:15:14 -0500

    enhance: center items within Book and Film

--- src/components/Film/Film.tsx
+++ src/components/Film/Film.tsx
@@ -13,7 +13,7 @@ export default function Film(props: Props) {
 
   return (
     // REFACTOR: move px-2 to global, align with book
-    <div className={`mgn-film flex justify-content-between ${className}`}>
+    <div className={`mgn-film flex justify-content-between align-items-center ${className}`}>
       <PageControl action='prev' onClick={() => {}} className='px-1' isShown={false} />
       <Storyboard />
       <PageControl action='next' onClick={() => {}} className='px-1' />
