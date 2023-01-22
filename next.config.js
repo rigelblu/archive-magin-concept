2eeb392175894adc04f081803c0d3ce9b86e973d
Tom Hosiawa <tom@rigelblu.com>
2023-01-21 20:43:01 -0500

    style | next.config: add todo for setting standalone output

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   images: { unoptimized: true }, // OPTIMIZE: disable when we switch to running server
   reactStrictMode: true,
   swcMinify: true, // use swc instead of terser for minification
+  // output: 'standalone', // TODO: determine what affect this has, if I should turn it on for docker builds
   trailingSlash: true, // allow url ending with traling slash
   typescript: { tsconfigPath: './tsconfig.dev-next.json' },
 
