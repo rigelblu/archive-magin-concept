d26ecde8ab8aeead2ed0da045b47ede5980fa08c
Tom Hosiawa <tom@rigelblu.com>
2023-02-12 21:12:31 -0500

    fix | MaginPreview: layout bg to white

--- src/pages/try-magin/1/index.tsx
+++ src/pages/try-magin/1/index.tsx
@@ -13,7 +13,7 @@ export default function MarginPreview() {
   const router = useRouter();
 
   return (
-    <MainLayout className='mgn-try-magin' layoutKind='app'>
+    <MainLayout className='mgn-try-magin bg-white' layoutKind='app'>
       {/* REFACTOR: convert into component, accept 4 children elements */}
       <div className='mgn-preview flex h-screen flex-col items-center justify-center'>
         <div className='mgn-step flex w-full flex-1 flex-col justify-between bg-yellow-rb-200 sm:max-h-[51rem] sm:max-w-[25rem]'>
