5abdad6db24c4912bb047d66baf3beef4a64ce09
Tom Hosiawa <tom@rigelblu.com>
2023-04-30 15:47:08 -0400

    feat | support canvasClassName on MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,17 +4,23 @@ import React from 'react';
 
 import Header from './Header';
 import Footer from './Footer';
-// TODO: add unit tests
 
 interface Props {
   children: React.ReactNode;
+  canvasClassName?: string;
   bodyClassName?: string;
   className?: string;
   layoutKind?: string;
 }
 
 export default function MainLayout(props: Props) {
-  const { children, bodyClassName = '', className = '', layoutKind = 'site' } = props;
+  const {
+    children,
+    bodyClassName = '',
+    canvasClassName = '',
+    className = '',
+    layoutKind = 'site',
+  } = props;
   let showHeader = false;
   let showFooter = false;
 
@@ -32,10 +38,12 @@ export default function MainLayout(props: Props) {
   }
 
   return (
-    <div className={`mx-auto max-w-4xl bg-yellow-rb-200 ${bodyClassName}`}>
-      {showHeader && <Header />}
-      <main className={className}>{children}</main>
-      {showFooter && <Footer />}
+    <div className={`mgn-canvas ${canvasClassName}`}>
+      <div className={`mgn-body mx-auto max-w-4xl bg-yellow-rb-200 ${bodyClassName}`}>
+        {showHeader && <Header />}
+        <main className={className}>{children}</main>
+        {showFooter && <Footer />}
+      </div>
     </div>
   );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -14,6 +14,7 @@ export default function Home() {
   return (
     <MainLayout
       bodyClassName='h-screen flex flex-col'
+      canvasClassName='bg-yellow-rb-200'
       className='flex flex-1 flex-col items-center justify-center py-1'
     >
       <div className='mgn-home flex flex-1 flex-col items-center justify-center'>
