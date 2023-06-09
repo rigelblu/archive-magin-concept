27ecfedf0d9241a1ec20d114ba4e3c8ae906f4f7
Tom Hosiawa <tom@rigelblu.com>
2023-06-09 12:30:38 -0400

    feat | show scene number

--- src/components/Film/ScenePanel.tsx
+++ src/components/Film/ScenePanel.tsx
@@ -3,16 +3,24 @@
 import Image from 'next/image';
 import SceneMarker from '@/components/SceneMarker/SceneMarker';
 
+import Locale from '@/locales/en.json';
+
 type Props = {
   className?: string;
 };
 
 export default function ScenePanel(props: Props) {
   const { className = '' } = props;
+  const locale = Locale as LocaleType;
   const sceneNum = 1;
 
   return (
     <div className={`mgn-scenepanel outline rounded-lg p-2 w-full ${className}`}>
+      {/* OPTIMIZE: find better font */}
+      <div className='text-xs'>
+        {Locale.film.scene}: {sceneNum}
+      </div>
+      <hr className='border-1 border-black my-1 py-1' />
       <SceneMarker sceneNum={sceneNum}>
         {/* OPTIMIZE: load image cdn */}
         {/* OPTIMIZE: create SceneImage component, accept alt and src props */}
