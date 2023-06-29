b5fac33692c9611b826f76f2bee3c59c830b526b
Tom Hosiawa <tom@rigelblu.com>
2023-06-28 21:04:08 -0400

    Use svgs instead ascii codes, display depending on props

--- src/components/PageControl/PageControl.tsx
+++ src/components/PageControl/PageControl.tsx
@@ -1,6 +1,8 @@
 // Copyright rigélblu inc.
 // All rights reserved.
 import { Button } from 'primereact/button';
+import IconArrowLeft from '@/assets/common/icons/arrow-left.svg';
+import IconArrowRight from '@/assets/common/icons/arrow-right.svg';
 
 interface Props {
   // REFACTOR: improve naming to indicate aria-label
@@ -13,28 +15,17 @@ interface Props {
 export default function PageControl(props: Props) {
   const { action, onClick, isShown = true, className = '' } = props;
 
-  // TODO: replace ascii with svg
-  let icon = '';
-  if (!isShown) {
-    icon = '';
-  } else if (action === 'prev') {
-    icon = 'pi-chevron-left';
-  } else if (action === 'next') {
-    icon = 'pi-chevron-right';
-  } else {
-    console.error('PageControl: invalid action');
-  }
-
   // REFACTOR: w-4 is used to prevent Book from shifting
-  // FIXME: use primereact button instead of div
   // TODO: style chevron in blue-rb-600 and background in blue-rb-lighter
   return (
     <Button
       className={`mgn-page-control p-button-text h-4 max-w-max ${className}`}
       aria-label={action}
       disabled={!isShown}
-      icon={`pi ${icon}`}
       onClick={onClick}
-    />
+    >
+      {isShown && action === 'prev' && <IconArrowLeft className='w-auto h-auto' />}
+      {isShown && action === 'next' && <IconArrowRight className='w-auto h-auto' />}
+    </Button>
   );
 }
