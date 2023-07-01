b3acab1f03de79845e4ce7536147f9d8e45b60b2
Tom Hosiawa <tom@rigelblu.com>
2023-07-01 07:35:10 -0400

    fix | border is shifting content

--- src/components/SceneMarker/SceneMarker.tsx
+++ src/components/SceneMarker/SceneMarker.tsx
@@ -3,19 +3,13 @@
 import styles from './SceneMarker.module.scss';
 
 type Props = {
-  className?: string;
   children: React.ReactNode;
+  className?: string;
 };
 
 // REFACTOR: rename to SceneIndicator
 export default function SceneMarker(props: Props) {
   const { children, className = '' } = props;
 
-  return (
-    <div
-      className={`${styles['mgn-scenemarker']} pl-1 border-l-2 border-l-blue-rb-600 ${className}`}
-    >
-      {children}
-    </div>
-  );
+  return <div className={`${styles['mgn-scenemarker']} relative ${className}`}>{children}</div>;
 }
