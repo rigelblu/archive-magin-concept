9583a823b4b18cfedc7128a0a0b307e47b7b27b6
Tom Hosiawa <tom@rigelblu.com>
2023-07-06 20:22:47 -0400

    refactor | use flexbox for allocating heights

--- src/pages/try-magin/3/index.tsx
+++ src/pages/try-magin/3/index.tsx
@@ -16,12 +16,11 @@ export default function MarginPreview() {
   const [scene, setScene] = useState(2);
   const startScene = 1;
   const endScene = 2;
-  const [deviceClasses, setDeviceClasses] = useState('');
 
   return (
     <MainLayout canvasClassName='bg-black' className='mgn-try-magin bg-white' layoutKind='app'>
       {/* REFACTOR: convert into component, accept 4 children elements */}
-      <div className='flex h-screen flex-col items-center justify-center bg-neutral-950 sm:max-w-[25rem]'>
+      <div className='flex h-screen flex-col items-center justify-center bg-neutral-950'>
         {/* HACK: have to use fixed rem for max height and width due to mobile browsers */}
         <div className='mgn-step flex max-h-[48rem] w-full flex-1 flex-col justify-between bg-yellow-rb-200 p-2 sm:max-w-[25rem]'>
           <div className='mgn-story flex w-full flex-1 flex-col justify-between'>
